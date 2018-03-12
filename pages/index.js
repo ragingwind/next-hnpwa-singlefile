8fbe5e046a895c176ebc785c6f7a69600576d079
Jimmy Moon <ragingwind@gmail.com>
2018-03-12 20:53:34 +0900

    fix indent

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 
 export default () =>
-	<div>
-		<Head>
-			<title>NEXT-PWA</title>
-			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-		</Head>
-		<p>Welcome to next.js with PWA!</p>
+<div>
+	<Head>
+		<title>NEXT-PWA</title>
+		<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+	</Head>
+	<p>Welcome to next.js with PWA!</p>
 </div>
