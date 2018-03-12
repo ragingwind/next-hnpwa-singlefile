108bd162642a9b84098bf5ba3d382ae7547d00f0
Jimmy Moon <ragingwind@gmail.com>
2018-03-12 22:47:43 +0900

    add next-workbox for sw

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,24 @@ import Head from 'next/head'
 import Link from "next/link";
 import 'isomorphic-unfetch'
 
-export default class extends React.Component {
+export default class extends React.PureComponent {
 	static async getInitialProps ({query, pathname}) {
 		const res = await fetch(`https://hnpwa.com/api/v0/${query.feed || 'news' }.json`)
 		return {feeds: await res.json()}
 	}
+
+	componentDidMount () {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/static/workbox/sw.js')
+        .then(registration => {
+          console.log('service worker registration successful')
+        })
+        .catch(err => {
+          console.warn('service worker registration failed', err.message)
+        })
+    }
+  }
 
 	render () {
 		return (
---
+++ server.js
@@ -0,0 +1,23 @@
+const {join} = require('path')
+const {createServer} = require('http')
+const next = require('next')
+
+const app = next({dev: false})
+const handle = app.getRequestHandler()
+const port = Number.parseInt(process.argv.pop().split('=')[1] || 3000)
+const root = process.cwd()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    if (req.url.startsWith('/static/')) {
+      app.serveStatic(req, res, join(root, `.${req.url}`))
+    } else {
+      handle(req, res, req.url)
+    }
+  }).listen(port, err => {
+    if (err) {
+      throw err
+    }
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
