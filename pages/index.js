05194ae5fbe2d03256a7fd9785385449e2dd449c
Jimmy Moon <ragingwind@gmail.com>
2018-03-12 21:26:52 +0900

    add manifest

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default () =>
 	<Head>
 		<title>NEXT-PWA</title>
 		<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+		<link rel="manifest" href="/static/manifest/manifest.json" />
 	</Head>
 	<p>Welcome to next.js with PWA!</p>
 </div>
