307bd42b668d4722a69e24bbe618f7c24aeca4c0
Jimmy Moon <ragingwind@gmail.com>
2018-03-12 21:38:32 +0900

    add navigation

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Head from 'next/head'
+import Link from "next/link";
 
-export default () =>
-<div>
-	<Head>
-		<title>NEXT-PWA</title>
-		<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-		<link rel="manifest" href="/static/manifest/manifest.json" />
-	</Head>
-	<p>Welcome to next.js with PWA!</p>
-</div>
+export default () => (
+	<div>
+		<Head>
+			<title>NEXT-PWA</title>
+			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			<link rel="manifest" href="/static/manifest/manifest.json" />
+		</Head>
+		<header>
+			<nav>
+				<Link href='/'><a>News</a></Link>
+				<Link href='/?feed=newest'><a>Newest</a></Link>
+				<Link href='/?feed=ask'><a>Ask</a></Link>
+				<Link href='/?feed=show'><a>Show</a></Link>
+				<Link href='/?feed=jobs'><a>Jobs</a></Link>
+			</nav>
+		</header>
+	</div>
+)
