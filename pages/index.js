d9a7f269cf13e772deffe000976c17546fe606c9
Jimmy Moon <ragingwind@gmail.com>
2018-03-12 16:42:28 +0900

    add skeleton code for page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-export default () => <div>Welcome to next.js with PWA!</div>
+import Head from 'next/head'
+
+export default () =>
+	<div>
+		<Head>
+			<title>NEXT-PWA</title>
+			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+		</Head>
+		<p>Welcome to next.js with PWA!</p>
+</div>
